777e085b20ed5807ee080bb722badd9ea0239a7d
Guzel Sharafutdinova <guzelsharafutdinova@icloud.com>
2018-09-16 17:34:11 +0300

    my homework

--- TestiOSLab.playground/Contents.swift
+++ TestiOSLab.playground/Contents.swift
@@ -1,131 +1,169 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+//--------------------------------Homework 2.2--------------------------------------------------------
 
-var str = "Hello, playground"
-var someString = "hi"
+//import Cocoa
+import Foundation
 
-str = "123"
-someString = "1"
 
-var someArray: Array<String> = ["value"]
-var someArray2 = [Int]()
 
-for _ in someArray {
-    print("Hello")
+class Unit {
+    
+    var health:Int
+    var damage:Int
+    var protection:Int
+    var dexterity:Int
+    var speed:Int
+    private var name:String
+    
+    func getName() -> String {
+        return self.name
+    }
+    
+    init(health:Int, damage:Int, protection:Int, dexterity:Int, speed:Int, name:String) {
+        self.health = health
+        self.damage = damage
+        self.protection = protection
+        self.dexterity = dexterity
+        self.speed = speed
+        self.name = name
+    }
+    
+    
+    func Attack(to enemy:Unit) {
+        let rand = Int(arc4random_uniform(100))
+        if rand > enemy.dexterity {
+            enemy.health = enemy.health - self.damage * (1 - enemy.protection / 100)
+        }
+    }
 }
 
-for value in someArray {
-    print("\(value)")
-}
-
-for index in 0 ..< someArray.count - 1 {
-    print("\(index)")
-}
-
-for value in someArray.enumerated() {
+class Magician:Unit {
     
 }
 
-someArray.forEach { (someString) in
-    print("printing")
+class Assasin:Unit {
+    
 }
 
-someArray.forEach{ print($0) }
-
-var someIntValue: Int? = nil
-
-someIntValue?.signum()
-
-if let newValue = someIntValue, newValue > 0 {
-    someIntValue = someIntValue! + 1
+class Knight:Unit {
+    
 }
 
-if let intValue = Int(str) {
-    someIntValue = intValue
-}
-
-
-func selectionSort(_ list:[Int]) -> [Int] {
+class Battlefield {
     
-    guard list.count > 1 else { return list }
+    func findCountOfHealthyFighters(arr:[Unit]) -> Int {
+        var countOfHealthyFighters = 0
+        for fighters in arr {
+            if fighters.health > 0 { countOfHealthyFighters = countOfHealthyFighters + 1 }
+        }
+        return countOfHealthyFighters
+    }
     
-    var mutableArray = list
     
-    for index in 0 ..< mutableArray.count - 1 {
+    
+    func beginBattle(array:[Unit]) {
         
-        var minIndex = index
+        var arr = array
+        let countOfHealthyFighters = findCountOfHealthyFighters(arr: arr)
         
-        for nextIndex in minIndex + 1 ..< mutableArray.count {
+        while countOfHealthyFighters > 0 {
             
-            if mutableArray[minIndex] < mutableArray[nextIndex] {
-                minIndex = nextIndex
+            var arr2:[Unit] = []
+            print("_________________")
+            print("Begin")
+            print("_________________")
+            var count:Int = 0
+            var index:Int = -1
+            var smbIndex = 0 //индекс борцов с health > 0
+            
+            for smb in arr {
+                index = index + 1
+                if smb.health > 0 {
+                    count = count+1
+                    smbIndex = index
+                    arr2.append(arr[smbIndex]) //массив борцов с health > 0
+                }
+            }
+            
+            if count == 1 {
+                print("The winner is ", arr[smbIndex].getName())
+                print("Participants: ")
+                var count:Int = 0
+                for k in array {
+                    count = count+1
+                    print(count, "–", k.getName())
+                }
+                break
+            } else {
+                var currentSpeed = 0
+                var currentIndex = -1
+                var smbdIndex = 0 // индекс борца с большей скоростью, он будет наносить урон
+                
+                for smbd in arr2 {
+                    
+                    smbd.speed = smbd.speed + Int(arc4random_uniform(5))
+                    currentIndex = currentIndex + 1
+                    if smbd.health <= 0 {
+                        arr2.remove(at: currentIndex)
+                    } else {
+                        
+                        if smbd.speed > currentSpeed && smbd.health > 0 {
+                            currentSpeed = smbd.speed
+                            smbdIndex = currentIndex
+                        }
+                    }
+                    
+                }
+                
+                
+                var i = Int(arc4random_uniform(UInt32(arr2.count))) //индекс рандомного борца из массива борцов с health > 0
+                
+                while i == smbdIndex { i = Int(arc4random_uniform(UInt32(arr2.count))) }
+                
+                if i != smbdIndex {
+                    arr[smbdIndex].Attack(to: arr2[i])
+                    print(arr2[i].health)
+                    print(arr[smbdIndex])
+                    print(arr2[i])
+                    if arr2[i].health <= 0 { arr.remove(at: i) }
+                }
             }
         }
-        if index != minIndex {
-            mutableArray.swapAt(index, minIndex)
+    }
+    
+}
+
+let mag = Magician(health: 100, damage: 20, protection: 30, dexterity: 40, speed: 1, name: "Mag")
+let assasin = Assasin(health: 100, damage: 40, protection: 30, dexterity: 60, speed:2, name: "As")
+let knight = Knight(health: 100, damage: 30, protection: 30, dexterity: 50, speed:3, name: "Kn")
+let arr = [mag, assasin, knight]
+let battle = Battlefield()
+battle.beginBattle(array: arr)
+
+
+//--------------------------------Homework 2.1--------------------------------------------------------
+
+
+
+
+//: Playground - noun: a place where people can play
+
+
+func mytrim(_ word: String, letter: String) -> String {
+    var cur: String = ""
+    for index in 0 ..< word.count {
+        let i = word.index(word.startIndex, offsetBy: index)
+        let current = String(word[i])
+        if current != letter {
+            cur += current
+            //print(current)
+            //print(cur)
         }
     }
-    return mutableArray
+    return cur
 }
 
-let list = [1,3,5,-2,123,0]
-
-selectionSort(list)
-
-protocol Human {
-    var name: String { get set }
-    var age: Int { get set }
-    
-    func speakHi()
-}
-
-
-class User: Human {
-    var name: String
-    var lastName: String = ""
-    var age: Int
-    var value: Character = "a"
-    
-    static let uniqId = 0
-    
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
-    
-    init(lastName: String) {
-        self.name = "Default"
-        self.age = 18
-        self.lastName = lastName
-    }
-    
-    func speakHi() {
-        print("Hi!")
-    }
-}
-
-class OldUser: User {
-    
-    override func speakHi() {
-        super.speakHi()
-        print("second hi")
-    }
-}
-
-struct Car {
-    var name: String
-    var cost: Double
-}
-
-let newCar = Car(name: "", cost: 1.2)
-
-
-let myUser = User(lastName: "Default")
-
-var dictionary: Dictionary<Int,Any> = [:]
-
-dictionary[0] = "String"
-dictionary[1] = 123
-
+//mytrim("Hello world", letter: "o")
+print(mytrim("Hello world", letter: "o"))
+print(mytrim("asdfasasdfs", letter: "a"))
 
